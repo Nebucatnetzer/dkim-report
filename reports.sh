b074cd65a22a6828d89962239b7442a2b4dcb73c
Andreas Zweili <andreas@zweili.ch>
2024-03-26 07:41:21 +0100

    Display version number

--- reports.sh
+++ reports.sh
@@ -4,6 +4,7 @@ mkdir -p "$WORKDIR"/xml
 mkdir -p "$WORKDIR"/html
 
 echo "converting reports to html"
+dmarc-report-converter --version
 dmarc-report-converter
 echo "open filewall port"
 sudo iptables -I INPUT -p tcp --dport $PORT -j ACCEPT
