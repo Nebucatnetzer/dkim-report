cfacdf4e58084799d3c2ca991fb1625a85359ff7
Andreas Zweili <andreas@zweili.ch>
2024-04-09 19:59:40 +0200

    Rewrite to flake-utils

--- flake.nix
+++ flake.nix
@@ -2,25 +2,21 @@
   description = "A simple flake to generate DKIM reports.";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
   outputs =
-    { self, nixpkgs }:
-    let
-      systems = [
-        "aarch64-darwin"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ];
-      forEachSystem = nixpkgs.lib.genAttrs systems;
-    in
     {
-      packages = forEachSystem (
-        system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        {
+      self,
+      flake-utils,
+      nixpkgs,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        packages = rec {
           dkim-report = pkgs.writeShellApplication {
             name = "reports";
             runtimeInputs = [
@@ -29,15 +25,12 @@
             ];
             text = (builtins.readFile ./reports.sh);
           };
-          default = self.packages.${system}.dkim-report;
-        }
-      );
-      apps = forEachSystem (system: {
-        dkim-report = {
-          type = "app";
-          program = "${self.packages.${system}.dkim-report}/bin/reports";
+          default = dkim-report;
         };
-        default = self.apps.${system}.dkim-report;
-      });
-    };
+        apps = rec {
+          dkim-report = flake-utils.lib.mkApp { drv = self.packages.${system}.dkim-report; };
+          default = dkim-report;
+        };
+      }
+    );
 }
